634ee453a15aecc487707b6572cb650cbe078476
huntthepast <sancalazuardi@gmail.com>
2023-09-15 18:01:46 +0700

    Add Locomotive JS scrolling

--- website/nozaracy_website/files/javascript/OnHoverBox.js
+++ website/nozaracy_website/files/javascript/OnHoverBox.js
@@ -9,34 +9,28 @@ const onGlobalMove = (e) => {
     mouseSprite.style.left = e.pageX - scrollLeft + 'px';
     mouseSprite.style.top = e.pageY - scrollTop + 'px';
 }
-const onMouseMove = (e, i) => {
+const onMouseMove = (e) => {
     const scrollLeft = (window.pageXOffset !== undefined) ? window.pageXOffset : (document.documentElement || document.body.parentNode || document.body).scrollLeft;
     const scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
-    title[i].style.left = e.pageX - scrollLeft + 'px';
-    title[i].style.top = e.pageY - scrollTop + 'px';
+    title.forEach((tiles, i) => {
+        title[i].style.left = e.pageX - scrollLeft + 'px';
+        title[i].style.top = e.pageY - scrollTop + 'px';
+    });
 }
 const onMouseEnter = (e, i) => {
     mouseSprite.style.transform = 'translate(-50%,-50%) scale(0)';
     title[i].style.transform = 'translate(-50%,-50%) scale(1)';
-    const scrollLeft = (window.pageXOffset !== undefined) ? window.pageXOffset : (document.documentElement || document.body.parentNode || document.body).scrollLeft;
-    const scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
-    title[i].style.left = e.pageX - scrollLeft + 'px';
-    title[i].style.top = e.pageY - scrollTop + 'px';
 }
 const onMouseLeave = (e, i) => {
     mouseSprite.style.transform = 'translate(-50%,-50%) scale(1)';
     title[i].style.transform = 'translate(-50%,-50%) scale(0)';
-    const scrollLeft = (window.pageXOffset !== undefined) ? window.pageXOffset : (document.documentElement || document.body.parentNode || document.body).scrollLeft;
-    const scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
-    title[i].style.left = e.pageX - scrollLeft + 'px';
-    title[i].style.top = e.pageY - scrollTop + 'px';
 }
 
+htmlDoc.addEventListener("mousemove", (e) => onMouseMove(e));
 htmlDoc.addEventListener("mousemove", (e) => onGlobalMove(e));
 htmlDoc.addEventListener("scroll", (e) => onGlobalMove(e));
 
 thumbs.forEach((thumb, i) => {
-    thumb.addEventListener("mousemove", (e) => onMouseMove(e, i));
     thumb.addEventListener("mouseenter", (e) => onMouseEnter(e, i));
     thumb.addEventListener("mouseleave", (e) => onMouseLeave(e, i));
 });
